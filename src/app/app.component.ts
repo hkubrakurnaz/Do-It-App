9fe6de535c36d76f8817d7f6b4e207c9921f3cb8
Kubra Kurnaz <hkubrakurnaz@gmail.com>
2020-08-19 12:59:42 +0300

    First Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,74 @@
-import { Component } from '@angular/core';
+import { Component ,OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {FormControl, FormGroupDirective, NgForm} from '@angular/forms';
+import {ErrorStateMatcher} from '@angular/material/core';
+import { HttpClient } from '@angular/common/http';
 
+export class MyErrorStateMatcher implements ErrorStateMatcher {
+  isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
+    const isSubmitted = form && form.submitted;
+    return !!(control && control.invalid && (control.dirty || control.touched || isSubmitted));
+  }
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'do-it-app';
+export class AppComponent implements OnInit {
+  firstFormGroup: FormGroup;
+  secondFormGroup: FormGroup;
+  denemeInput="ali";
+  isEditable = false;
+  goalsForm: FormGroup;
+  goalsForm1: FormGroup;
+  prices: number[] = [
+    25,50,100,200,500
+   ];
+
+   emailFormControl = new FormControl('', [
+    Validators.required,
+    Validators.email,
+  ]);
+
+  matcher = new MyErrorStateMatcher();
+   
+  constructor(private _formBuilder: FormBuilder,private http: HttpClient) {
+
+  }
+
+  ngOnInit() {
+    this.goalsForm = new FormGroup({
+      goal: new FormControl('',Validators.required),
+      date: new FormControl('',Validators.required),
+      price: new FormControl('',Validators.required),
+      supervisorEmail: new FormControl('',Validators.required),
+      yourEmail: new FormControl('',Validators.required)
+    })
+
+    this.goalsForm1 = new FormGroup({
+      goal: new FormControl('',Validators.required),
+      date: new FormControl(new Date(),Validators.required),
+      price: new FormControl(150,Validators.required),
+      supervisorEmail: new FormControl('albayrakorhann@gmail.com',Validators.required),
+      yourEmail: new FormControl('',Validators.required)
+    })
+
+    
+    this.firstFormGroup = this._formBuilder.group({
+      firstCtrl: ['']
+    });
+    this.secondFormGroup = this._formBuilder.group({
+      secondCtrl: ''
+    });
+  }
+
+  click(){
+    console.log(this.goalsForm.value);
+  }
+
+  getUrl(){
+    return `url('../assets/images/1.jpeg')`;
+  }
 }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,37 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatStepperModule } from '@angular/material/stepper';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatSelectModule } from '@angular/material/select';
+import { HttpClientModule } from '@angular/common/http';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatFormFieldModule,
+    ReactiveFormsModule,
+    FormsModule,
+    MatInputModule,
+    MatStepperModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatSelectModule,
+    HttpClientModule,
+    MatButtonModule,
+    MatIconModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
+
