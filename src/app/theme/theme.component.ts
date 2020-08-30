86a80e1b01b9a725633c90a4ca3c6cca25e09029
Kubra Kurnaz <hkubrakurnaz@gmail.com>
2020-08-30 23:32:06 +0300

    change ui

--- src/app/theme/theme.component.ts
+++ src/app/theme/theme.component.ts
@@ -31,6 +31,13 @@ export class ThemeComponent implements OnInit {
     25,50,100,200,500
    ];
 
+   months: string[] = [
+    "01","02","04","05","06","07","08","09","10","11","12"
+     ];
+     years: string[] = [
+      "2020","2021","2022","2023","2024","2025","2026","2027","2028","2029","2030"
+     ]
+
   matcher = new MyErrorStateMatcher();
   
   //get formArray(): AbstractControl | null { return this.formGroup.get('formArray'); }
@@ -65,8 +72,8 @@ export class ThemeComponent implements OnInit {
         Validators.minLength(16), 
         Validators.maxLength(16)]],
        
-      cardHolder: ['',[Validators.required, 
-         Validators.pattern( '^[a-zA-Z ]*$')]],
+      // cardHolder: ['',[Validators.required, 
+      //    Validators.pattern( '^[a-zA-Z ]*$')]],
       expirationMonth: ['',[]],
       expirationYear:['',[]],
       cvc:['',[Validators.required,
@@ -116,7 +123,6 @@ export class ThemeComponent implements OnInit {
       error => console.log('oops', error));
   }
   calculateRemainDate(){
-    debugger;
     var startDate = new Date( this.dateForm.get('dateCtrl').value);
     var endDate =   new Date();              // Today
     var ndays;
@@ -145,6 +151,10 @@ export class ThemeComponent implements OnInit {
   }
   get userFormField() {
     return (<FormGroup>this.userEmailForm.get('field'));
+  }
+
+  public errorHandling = (control: string, error: string) => {
+    return this.creditCardForm.controls[control].hasError(error);
   }
 
   //  checkSameEmail(control:AbstractControl):{[key:string]:boolean} | null{
